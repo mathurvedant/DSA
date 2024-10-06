dd0c4089249dee0744b8739c6e707c381125a628
Vedant Mathur <mathurvedant89@gmail.com>
2024-10-06 18:04:12 -0400

    Initial Directory Struture and Binary Tree Plumbing

---
+++ include/binary_tree.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Vedant Mathur
+ *
+ * Binary Tree Data Structure Operations
+ */
+
+#include <stdint.h>
+
+typedef struct BTNode_ {
+    uint64_t key;
+    struct BTNode_ *left;
+    struct BTNode_ *right
+} BTNode;
+
+BTNode *in_order_traversal(BTNode *root);
+BTNode *pre_order_traversal(BTNode *root);
+BTNode *post_order_traversal(BTNode *root);
+BTNode *level_order_traversal(BTNode *root);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/binary_tree.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Vedant Mathur
+ *
+ * Binary Tree Data Structure Operations Implementation.
+ */
+
+#include <binary_tree.h>
+
+BTNode *in_order_traversal(BTNode *root)
+{}
+
+BTNode *pre_order_traversal(BTNode *root)
+{}
+
+BTNode *post_order_traversal(BTNode *root)
+{}
+
+BTNode *level_order_traversal(BTNode *root)
+{}
+
