1555f98b2c442a191475795a173f27252e211860
Vedant Mathur <mathurvedant89@gmail.com>
2024-10-07 21:20:02 -0400

    Driver program

---
+++ bin/driver.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Vedant Mathur
+ *
+ * DSA driver program.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <binary_tree.h>
+
+int main(void)
+{
+    printf("\nWelcome to DSA Driver Program!");
+}
+
