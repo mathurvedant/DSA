1fa1ad6ee11c380dce93fbb37a8358fa7ef452fd
Vedant Mathur <mathurvedant89@gmail.com>
2024-10-19 12:23:46 -0400

    Linear and Binary Search

---
+++ include/algos.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Vedant Mathur
+ *
+ * Various Computer Science Algorithms
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int linear_search_arr(uint64_t *arr, int arr_len,  uint64_t target);
+int binary_search_arr(uint64_t *arr, int arr_len, uint64_t target);
+
