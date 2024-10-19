7b0de7e71fe285224cf892633d5ffb506cc1a03d
Vedant Mathur <mathurvedant89@gmail.com>
2024-10-19 11:33:50 -0400

    Add Algos Driver Program

---
+++ bin/algos_driver.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 Vedant Mathur
+ *
+ * Algorithms driver program.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <errno.h>
+
+
+int main(void)
+{
+    printf("\nWelcome to Algorithms Driver Program!");
+
+    return 0;
+}
