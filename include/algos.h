c696f7ba8256146897167fe2c3e7e330225513b6
Vedant Mathur <mathurvedant89@gmail.com>
2024-10-28 12:00:29 -0400

    Sorting: bubble, selection, insertion, quick, merge

--- include/algos.h
+++ include/algos.h
@@ -11,3 +11,10 @@
 int linear_search_arr(uint64_t *arr, int arr_len,  uint64_t target);
 int binary_search_arr(uint64_t *arr, int arr_len, uint64_t target);
 
+void bubble_sort(int64_t *arr, int len);
+void selection_sort(int64_t *arr, int len);
+void insertion_sort(int64_t *arr, int len);
+void quick_sort(int64_t *arr, int len);
+void merge_sort(int64_t *arr, int len);
+
+
